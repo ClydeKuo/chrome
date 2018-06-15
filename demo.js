0889d708f6c04f4de801fae72e412d36c6b9345a
ck <clyde_guo@cloud-hr.com.cn>
2018-06-15 15:06:36 +0800

    单个

--- demo.js
+++ demo.js
@@ -16,75 +16,73 @@ function randomNum(minNum, maxNum) {
 }
 (async () => {
   const browser = await puppeteer.launch({
-    // Launch chromium using a proxy server on port 9876.
     // More on proxying:
     //    https://www.chromium.org/developers/design-documents/network-settings
-    args: ["--proxy-server=127.0.0.1:10005"]
+    //https://peter.sh/experiments/chromium-command-line-switches/
+    args: ["--proxy-server=127.0.0.1:10005",
+  "--user-agent='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.3964.2 Safari/537.36'"]
   });
   const newPagePromise = () => {
     return new Promise(resolve => {
-      browser.once("targetcreated", target => {
-        resolve(target.page());
-      });
-    });
-  };
-  const  screenshot= (page) => {
-    return new Promise(resolve => {
-      page.once("load", () => {
-        resolve(page.screenshot({ path: "example.png", fullPage: true }))
+      browser.once("targetcreated", async target => {
+        try {
+          console.log(target.url());
+          let newPage = await browser.newPage();
+          await newPage.setViewport({ width: 1920, height: 1048 });
+          await newPage.goto(target.url(), { waitUntil: "networkidle0" });
+          resolve(newPage);
+        } catch (e) {
+          console.log(e);
+        }
       });
     });
   };
   const page = await browser.newPage();
-  await page.setUserAgent(
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36"
-  );
   await page.setViewport({ width: 1920, height: 1048 });
   try {
-    await page.goto(
-      "http://sh-fuyang.com/",
-      // "https://www.baidu.com/s?ie=utf-8&f=3&rsv_bp=1&rsv_idx=2&tn=baiduhome_pg&wd=dd电机&rsv_spt=1&oq=win10%2520host&rsv_pq=de14753500005011&rsv_t=d99cUKzMeh3HklMUGj1i%2B5YBUs3o4LqMNlYy3iBZLxgcMGDqnRQc4Yxb0SExwjM5mn2%2F&rqlang=cn&rsv_enter=1&inputT=1791&rsv_sug3=17&rsv_sug1=17&rsv_sug7=100&rsv_sug2=1&prefixsug=ddd&rsp=1&rsv_sug4=3009",
-      { waitUntil: "networkidle2" }
+    await page.goto("http://sh-fuyang.com/", { waitUntil: "networkidle2" });
+    await page.screenshot({ path: "home.png", fullPage: true });
+    console.log("点击页面广告");
+    await page.click("#_3di13bpetea", { delay: 100 });
+    console.log("跳转到百度");
+    const baidu = await newPagePromise();
+    await baidu.screenshot({ path: "baidu1.png", fullPage: true });
+    console.log("点击input框");
+    await baidu.mouse.click(300, 30, { delay: 100 });
+
+    console.log("全选");
+    await baidu.keyboard.down("ControlLeft");
+    await baidu.keyboard.press("KeyA");
+    await baidu.keyboard.up("ControlLeft");
+
+    console.log("删除现有的文字");
+    await baidu.keyboard.press("Backspace");
+    console.log("输入热词");
+    await baidu.keyboard.type("收益", { delay: 100 });
+    await baidu.click('input[value="百度一下"][type=submit]', { delay: 100 });
+    await sleep("3s");
+    await baidu.screenshot({ path: "baidu2.png", fullPage: true });
+    console.log("点击百度广告");
+    let adList = await baidu.$$(
+      '[data-click][style="display:block !important;visibility:visible !important"]'
     );
-    // console.log(pages)
-    /* browser.once("targetcreated", target => {
-      console.log(1)
-    }) */
-    // await page.mouse.click(100, 1950, { delay: 0 });
-    // await sleep("10s");
-    await page.click("#iframe4341660_0", { delay: 100 });
-    console.log("click");
-    const newPage = await newPagePromise();
-    // console.log("targetcreated")
-    // await newPage.setViewport({ width: 1920, height: 1048 });
-    await screenshot(newPage)
-    // await sleep("3s");
+    let adIdList = adList.map(item => {
+      return item._remoteObject.description.match(/#(\w*?)\./)[1];
+    });
+    console.log(adIdList);
+    for (let i = 0; i < adIdList.length; i++) {
+      await baidu.click('[id="' + adIdList[i] + '"] a', { delay: 100 });
+      console.log("进入广告页面");
+      let custom = await newPagePromise();
+      let old = new Date();
+      for (let i = 0; i < 200; i++) {
+        await custom.mouse.move(randomNum(0, 1920), randomNum(0, 1000));
+      }
+      console.log(new Date() - old);
+      await custom.screenshot({ path: `custom${i}.png`, fullPage: true });
+    }
   } catch (e) {
     console.log(e);
   }
   await browser.close();
-
-  //   百度广告
-  //   $('[data-click][style="display:block !important;visibility:visible !important"]')
-  // Type into search box.
-  //   await page.click("#kw", { delay: 100 });
-  // page.mouse.click(300,30, { delay: 100 })
-  //全选
-  /* await page.keyboard.down("ControlLeft");
-  await page.keyboard.press("KeyA");
-  await page.keyboard.up("ControlLeft");
-  //删除现有的文字
-  await page.keyboard.press("Backspace");
-  //输入
-  await page.keyboard.type("医院", { delay: 100 });
-  await page.click('input[value="百度一下"][type=submit]', { delay: 100 });
-  let old=new Date()
-  for(let i=0;i<1;i++){
-    await page.mouse.move(randomNum(0,1920),randomNum(0,2500))
-  }
-  console.log(new Date()-old)
-  await sleep("3s");
-  
-  await page.screenshot({ path: "example.png" ,fullPage:true});
-  await browser.close(); */
 })();
