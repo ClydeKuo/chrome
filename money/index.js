195af0802c5cfc978550a53538a0bb24bd6e528f
ck <clyde_guo@cloud-hr.com.cn>
2018-07-11 16:25:32 +0800

    优化

--- money/index.js
+++ money/index.js
@@ -5,18 +5,24 @@ const chalk = require("chalk");
 const sleep = require("ko-sleep");
 const randomNum=require('../random')
 const agent = require('secure-random-user-agent')
+
 const newPagePromise = (browser) => {
   return new Promise((resolve, reject) => {
     //捕获不了弹窗时报错
     let t = setTimeout(() => {
-      reject({Timeout:true});
-    }, 1000);
+      browser.newPage();
+    }, 3000);
     browser.once("targetcreated", async target => {
       try {
-        console.log(target.url().substr(0, 100));
-        let newPage = await browser.newPage();
+        clearTimeout(t);
+        let url=target.url().substr(0, 200)
+        console.log(url)
+        let newPage = await target.page()
         await newPage.setViewport({ width: 1920, height: 1048 });
-        await newPage.goto(target.url(), { waitUntil: "networkidle0" });
+        if(url==="about:blank"){
+          newPage.timeout=true
+          newPage.text="timeout"
+        }
         resolve(newPage);
       } catch (e) {
         reject(e);
@@ -26,20 +32,36 @@ const newPagePromise = (browser) => {
     });
   });
 };
+const scroll=async page=>{
+  try{
+    //浏览网页
+   let old = new Date();
+   for (let j = 0; j < 100; j++) {
+     await page.mouse.move(randomNum(0, 1920), randomNum(0, 600));
+   }
+   await sleep("1s");
+   console.log((new Date() - old) / 1000 + "s");
+  }catch(e){
+    throw e
+  }
+}
 const surfing = async (ip,url) => {
+  let time1=new Date()
   const browser = await puppeteer.launch({
     // More on proxying:
     //    https://www.chromium.org/developers/design-documents/network-settings
     //https://peter.sh/experiments/chromium-command-line-switches/
     args: [
       // `--proxy-server=${ip}`,
-      `--user-agent=${agent()}`
-    ]
+      `--user-agent=${agent()}`,
+    ],
+    // headless: false,
   });
   const homePage = await browser.newPage();
   await homePage.setViewport({ width: 1920, height: 1048 });
   try {
-    await homePage.goto(url, { waitUntil: "networkidle0" });
+    await homePage.goto(url);
+    await scroll(homePage)
     if (!fs.existsSync(`${__dirname}/images/`)) {
       fs.mkdirSync(`${__dirname}/images/`);
     }
@@ -50,25 +72,40 @@ const surfing = async (ip,url) => {
     console.log("drawn homePage");
     let customs=[]
     for(let i=0;i<10;i++){
-      customs[i]=await homePage.mouse.click(randomNum(500, 1500), randomNum(200, 600), { delay: 100 });
-      console.log(customs[i])
+      await sleep("1s");
+      await homePage.mouse.click(randomNum(500, 1500), randomNum(200, 1000));
+      customs[i]=await newPagePromise(browser);
+      if(!customs[i].timeout){
+        console.log(i)
+        await scroll(customs[i])
+        await customs[i].screenshot({
+          path: `${__dirname}/images/customs-${i}-${ip.split(":")[0]}.png`,
+          fullPage: true
+        });
+
+        //点击网页
+        let sub=[]
+        for(let k=0;k<5;k++){
+          await customs[i].mouse.click(randomNum(500, 1500), randomNum(100, 800));
+          sub[k]= await newPagePromise(browser);
+          if(!sub[k].timeout){
+            await scroll(sub[k])
+            console.log("click custom page")
+            await sub[k].close()
+            break; 
+          }
+          await sub[k].close()
+        }
+      }else{
+        console.log(i+":"+customs[i].text)
+      }
+      await customs[i].close()
     }
-   /*  await homePage.mouse.click(300, 230, { delay: 100 });
-    let custom = await newPagePromise(browser);
-    let old = new Date();
-    for (let i = 0; i < 200; i++) {
-      await custom.mouse.move(randomNum(0, 1920), randomNum(0, 1000));
-    }
-    console.log((new Date() - old) / 1000 + "s");
-    await custom.screenshot({
-      path: `${__dirname}/images/sg-qq-${ip.split(":")[0]}.png`,
-    });
-    console.log("drawn qq-sg");
-    console.log(chalk.green(ip)); */
   } catch (e) {
     console.log(chalk.red(ip));
     console.log(chalk.red(e));
   } finally {
+    console.log((new Date() - time1) / 1000 + "s");
     await browser.close();
   }
 };
