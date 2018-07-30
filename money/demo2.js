94dfdd290750da7d0404c51816fd514137a026e4
ck <clyde_guo@cloud-hr.com.cn>
2018-07-30 17:52:36 +0800

    fewa

--- money/demo2.js
+++ money/demo2.js
@@ -6,23 +6,9 @@ const sleep = require("ko-sleep");
 const agent = require('secure-random-user-agent')
 const randomNum=require('../lib/random')
 const getIp=require("../lib/getip")
+const formatDateTime =require("../lib/formatDateTime")
+const scroll =require("../lib/scroll.js")
 
-
-const formatDateTime = function () {  
-  let date=new Date()
-  var y = date.getFullYear();  
-  var m = date.getMonth() + 1;  
-  m = m < 10 ? ('0' + m) : m;  
-  var d = date.getDate();  
-  d = d < 10 ? ('0' + d) : d;  
-  var h = date.getHours();  
-  h=h < 10 ? ('0' + h) : h;  
-  var minute = date.getMinutes();  
-  minute = minute < 10 ? ('0' + minute) : minute;  
-  var second=date.getSeconds();  
-  second=second < 10 ? ('0' + second) : second;  
-  return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;  
-};  
 const surfing = async (ip,url) => {
   console.log(`${chalk.green(formatDateTime())}:${chalk.yellow(`start:${url}`)}`)
   console.time("surfing") 
@@ -43,7 +29,8 @@ const surfing = async (ip,url) => {
   try {
     await homePage.goto(url, { waitUntil: "domcontentloaded" });
     await (await browser.pages())[0].close()  //关闭第一个空页面
-    await sleep("10s");
+    await scroll(homePage)
+    await sleep("3s");
     /* await homePage.screenshot({
       path: `${__dirname}/images/homePage-${ip.split(":")[0]}.${new Date().getTime()}.png`,
       fullPage: true
@@ -52,28 +39,35 @@ const surfing = async (ip,url) => {
     for(let i=0;i<10;i++){
       await sleep("1s");
       await homePage.mouse.click(randomNum(500, 1500), randomNum(200, 1000), { delay: 100 });
+      if((await browser.pages()).length>3){
+        break;
+      }
     }
     let pages=await browser.pages()
     if(pages.length<2){
       throw "没有点击事件"
     }else{
-      await sleep("50s");
-      if (!fs.existsSync(`${__dirname}/images/`)) {
+      await sleep("20s");
+      /* if (!fs.existsSync(`${__dirname}/images/`)) {
         fs.mkdirSync(`${__dirname}/images/`);
-      }
+      } */
       for(let i=0;i<pages.length;i++){
-        if([1,3,6].includes(i)){
+        await scroll(pages[i])
+        if([1,3].includes(i)){
           for(let j=0;j<5;j++){
             await pages[i].mouse.click(randomNum(500, 1500), randomNum(200, 1000), { delay: 100 });
           }
         }
-        console.log(i)
-        await pages[i].screenshot({
+        console.log(`${chalk.green(formatDateTime())}:png:${i}`)
+        /* await pages[i].screenshot({
           path: `${__dirname}/images/page-${ip.split(":")[0]}.${new Date().getTime()}.png`,
           fullPage: true
-        });
-        pages[i].close()
+        }); */
+        console.log(`${chalk.green(formatDateTime())}:after png:${i}`)
+        await pages[i].close()
+        console.log(`${chalk.green(formatDateTime())}:close png:${i}`)
       }
+      await sleep("20s");
     }
   } catch (e) {
     console.log(`${chalk.green(formatDateTime())}:${chalk.red(ip)}`)
