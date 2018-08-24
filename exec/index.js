f186c336f0a102c879646d781ae33e7c77512975
ck <clyde_guo@cloud-hr.com.cn>
2018-08-24 17:55:06 +0800

    发二娃水电费

--- exec/index.js
+++ exec/index.js
@@ -12,9 +12,11 @@ var exec = require("child_process").exec;
 var moment=require("moment")
 const fs = require('fs')
 const sleep = require("ko-sleep");
+const formatDateTime =require("../lib/formatDateTime")
+const chalk = require("chalk");
 // vmrun的命令
 const execsync = cmdStr => {
-  console.log(cmdStr)
+  console.log(`${chalk.green(formatDateTime())}:${cmdStr}`)
   return new Promise((resolve, reject) => {
     exec(cmdStr, function(err, stdout, stderr) {
       if (err) {
@@ -44,8 +46,8 @@ const getMax=dir=>{
   }else{
     return false
   }
-  
 }
+
 const init = async () => {
     try{
         let date=moment().format('MM-DD')
@@ -57,17 +59,22 @@ const init = async () => {
         for(let i=max+1;i<max+100;i++){
           console.time("install")
           let newVM=`${dir}${date}-${i}\\${date}-${i}.vmx`
+          //克隆虚拟机的快照
           let cmdStr = `vmrun -T ws  clone  "E:\\machines\\xp3\\xp3.vmx" ${newVM} linked -snapshot=init7  -cloneName=${date}-${i}`;
           await execsync(cmdStr);
-          let cmdStr2 = `vmrun -T ws start "${newVM}"`;
+          // 启动虚拟机
+          let cmdStr2 = `vmrun -T ws start "${newVM}" nogui`;
           await execsync(cmdStr2);
           await sleep("20s");
+          // 牛B修改
           let cmdStr3=`vmrun  -gu Administrator -gp 1  -T ws runProgramInGuest   "${newVM}" "C:\\Documents and Settings\\Administrator\\桌面\\modify.exe" `
           await execsync(cmdStr3);
           await sleep("100s");
+          // 静默安装软件
           let cmdStr4=`vmrun  -gu Administrator -gp 1  -T ws runProgramInGuest   "${newVM}" "C:\\Documents and Settings\\Administrator\\桌面\\7654静默包\\7654静默包.exe" `
           await execsync(cmdStr4);
           await sleep("20s");
+          // 关机
           let cmdStr5 = `vmrun -T ws stop "${newVM}" soft`;
           await execsync(cmdStr5);
           console.timeEnd('install');
